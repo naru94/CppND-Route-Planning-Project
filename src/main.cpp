3ba4be2c3fa6ae97b217a8af9d2e5f973aaa2293
naru94 <99nimesh99@gmail.com>
2020-05-27 02:46:37 +0530

    Maps changed

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@ int main(int argc, const char **argv)
     else {
         std::cout << "To specify a map file use the following format: " << std::endl;
         std::cout << "Usage: [executable] [-f filename.osm]" << std::endl;
-        osm_data_file = "../map.osm";
+        osm_data_file = "../map_example.osm";
     }
     
     std::vector<std::byte> osm_data;
